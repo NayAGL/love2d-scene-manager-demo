050be27885b20f516ce26ee8e339eb8a8c4595f1
NayAGL <nayaglatt25@gmail.com>
2025-02-11 23:24:47 -0500

    Added enemy to the scene and fixed comments.

--- gameScene.lua
+++ gameScene.lua
@@ -1,11 +1,15 @@
 gameScene = {} -- Creates an empty table.
 
 local config = require("config") -- Imports variables from (config.lua).
-local Player = require("Player") -- Imports the button class.
+local Player = require("Player") -- Imports the player class file.
+local Enemy = require("Enemy") 
 
--- Create player here.
+-- Player. (Some variables will be assigned manually later.)
 player = Player:new(config.playerX, config.playerY, config.playerRadius,
-                       config.playerSpeed, config.playerSprint)
+                    config.playerSpeed, config.playerSprint)
+-- Enemy.
+enemy = Enemy:new(config.enemyX, config.enemyY, config.enemyRadius,
+                  config.enemySpeed)
 
 function gameScene.load()
   love.window.setTitle("Game Scene") -- Might remove.
@@ -13,10 +17,12 @@ end
 
 function gameScene.update(dt)
   player:update(dt)
+  enemy:update(dt, player)
 end
 
 function gameScene.draw()
   player:draw()
+  enemy:draw()
 end
 
 return gameScene -- Returns the table.
